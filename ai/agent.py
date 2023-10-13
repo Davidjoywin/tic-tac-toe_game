7903d844c3d7995bebae4996e1d294fade4e3f06
David <davidjoy.win@gmail.com>
2023-10-13 03:35:07 +0100

    ai agent fully done

--- ai/agent.py
+++ ai/agent.py
@@ -1,51 +1,68 @@
+import random
+
 from lib import winPos
 
 
 class Agent:
-	def __init__(self):
-		self.weight = {}
+    def __init__(self):
+        self.weight = {}
+        self.opp = 1
+        self.free = 1
 
-	def buildWeight(self, board):
-		win_pos = winPos()
-		for i in range(3):
-			for j in range(3):
-				pos = (i, j)
-				is_visited = pos in visited
+    def buildWeight(self, board, cur_player, players):
+        win_pos = winPos()
+        for i in range(3):
+            for j in range(3):
+                pos = (i, j)
+                pos_move = Agent.getPotMove(board, pos, players)
+                if pos_move:
+                    pos_list = Agent.joinPotMovesSides(pos_move, pos)
+                    self.assignWeight(board, pos, pos_list, cur_player, players)
 
-	@staticmethod
-	def getPotMove(board, pos, player_1, player_2):
-		"""
-		getPotMove: get potential/possible best moves of a player
-		
-		return: list of win-pos (win-pos is a list of potential win in tuple(x,y))
-		"""
 
-		x, y = pos
-		if board[x][y] == player_1 or board[x][y] == player_2:
-			return None
+    @staticmethod
+    def getPotMove(board, pos, players):
+        """
+        getPotMove: get potential/possible best moves of a player
 
-		else:
-			pot = []
-			for win_pos in winPos():
-				if pos in win_pos:
-					pot.append(win_pos)
-			return pot
+        return: list of win-pos (win-pos is a list of potential win in tuple(x,y))
+        """
 
-	@staticmethod
-	def joinPotMovesSides(potMoves, cur_pos):
-		pos_list = []
-		for pos in pot:
-			pos_list.append(pos)
-		
-		# join all the win pos together in a list and
-		# exempt the pos whose weight is being computed
+        x, y = pos
+        if board[x][y] in players:
+            return None
 
-		return list(filter(
-			pos_list, lambda val: if val == cur_pos
-		))
+        else:
+            pot = []
+            for win_pos in winPos():
+                if pos in win_pos:
+                    pot.append(win_pos)
+            return pot
 
-	def assignWeight(board, cur_pos, potMoves, player_1, player_2):
-		
-		for pos in potMoves:
-			x, y = pos
-			
+    @staticmethod
+    def joinPotMovesSides(potMoves, cur_pos):
+        pos_list = []
+        for pos in potMoves:
+            pos_list.extend(pos)
+
+        # join all the win pos together in a list and
+        # exempt the pos whose weight is being computed
+
+        return list(filter(
+            lambda val: val != cur_pos, pos_list
+            ))
+
+    def assignWeight(self, board, cur_pos, potMoves, cur_player, players):
+        self.weight[cur_pos] = 0
+        for pos in potMoves:
+            x, y = pos
+            if board[x][y] in players:
+                self.weight[cur_pos] += self.opp + random.random()
+                print(self.weight[cur_pos])
+
+            elif board[x][y] == cur_player:
+                self.weight[cur_pos] += self.opp + random.random()
+                print(self.weight[cur_pos])
+            else:
+                print(self.weight[cur_pos])
+                self.weight[cur_pos] += self.free
--- ai/test.py
+++ ai/test.py
@@ -10,5 +10,34 @@ agent = Agent()
 
 board = [['-' for _ in range(3)] for _ in range(3)]
 
-pot = Agent.getPotMove(board, (0,0), 'x', 'o')
-print(pot)
+board[1][1] = 'x'
+board[2][2] = 'o'
+
+# print(board)
+for i in range(3):
+    for j in range(3):
+        pot = Agent.getPotMove(board, (i, j), ('x', 'o'))
+        # print(pot)
+
+pot = Agent.getPotMove(board, (0,1), ('x', 'o'))
+sides = Agent.joinPotMovesSides(pot, (0,1))
+# print(sides)
+
+board[0][1] = 'x'
+board[2][0] = 'o'
+board[0][2] = 'x'
+board[0][0] = 'o'
+board[2][1] = 'x'
+
+board = [['-' for _ in range(3)] for _ in range(3)]
+board[1][1] = 'o'
+
+for i in range(3):
+    for j in range(3):
+        print(board[i][j], end=' ')
+    print()
+agent.buildWeight(board, 'x', ('x', 'o'))
+
+print(agent.weight)
+
+print(max(agent.weight.values()))
